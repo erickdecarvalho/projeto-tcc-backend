875ae60bc46dae1b019e150eae6f57511f69d5ad
erick7amorim <erickamorimcarv@gmail.com>
2024-09-05 22:21:48 -0300

    backend project created

---
+++ api-marketplace/src/test/java/br/com/apimarketplace/ApiMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apimarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
