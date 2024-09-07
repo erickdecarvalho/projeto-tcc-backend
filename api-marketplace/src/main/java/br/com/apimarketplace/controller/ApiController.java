474f69222c48bacd10a841ddced67b7e6140a2f4
erick7amorim <erickamorimcarv@gmail.com>
2024-09-06 21:37:18 -0300

    feat(Api): crud completed

--- api-marketplace/src/main/java/br/com/apimarketplace/controller/ApiController.java
+++ api-marketplace/src/main/java/br/com/apimarketplace/controller/ApiController.java
@@ -1,15 +1,14 @@
 package br.com.apimarketplace.controller;
 
 import br.com.apimarketplace.dto.CreateApiDto;
+import br.com.apimarketplace.dto.UpdateApiDto;
 import br.com.apimarketplace.model.Api;
 import br.com.apimarketplace.service.ApiService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/apis")
@@ -25,5 +24,35 @@ public class ApiController {
     public ResponseEntity<Api> createApi(@RequestBody CreateApiDto createApiDto) {
         var apiId = apiService.createApi(createApiDto);
         return ResponseEntity.created(URI.create("/apis/" + apiId.toString())).build();
+    }
+
+    @GetMapping("/{apiId}")
+    public ResponseEntity<Api> getApiById(@PathVariable("apiId") String userId) {
+        var api = apiService.getApiById(userId);
+
+        if (api.isPresent()) {
+            return ResponseEntity.ok(api.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Api>> listApis() {
+        var apis = apiService.listApis();
+
+        return ResponseEntity.ok(apis);
+    }
+
+    @PutMapping("/{apiId}")
+    public ResponseEntity<Void> updateApi(@PathVariable("apiId") String id, @RequestBody UpdateApiDto updateApiDto) {
+        apiService.updateApiById(id, updateApiDto);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{apiId}")
+    public ResponseEntity<Void> deleteApi(@PathVariable("apiId") String id) {
+        apiService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- api-marketplace/src/main/java/br/com/apimarketplace/service/ApiService.java
+++ api-marketplace/src/main/java/br/com/apimarketplace/service/ApiService.java
@@ -1,10 +1,13 @@
 package br.com.apimarketplace.service;
 
 import br.com.apimarketplace.dto.CreateApiDto;
+import br.com.apimarketplace.dto.UpdateApiDto;
 import br.com.apimarketplace.model.Api;
 import br.com.apimarketplace.repository.ApiRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -27,5 +30,55 @@ public class ApiService {
 
         var savedApi = apiRepository.save(api);
         return savedApi.getId();
+    }
+
+    public Optional<Api> getApiById(String apiId) {
+        return apiRepository.findById(UUID.fromString(apiId));
+    }
+
+    public List<Api> listApis() {
+        return apiRepository.findAll();
+    }
+
+    public void updateApiById(String userId, UpdateApiDto updateApiDto) {
+        var id = UUID.fromString(userId);
+
+        var apiEntity = apiRepository.findById(id);
+
+        if (apiEntity.isPresent()) {
+            var api = apiEntity.get();
+
+            if (updateApiDto.categoryId() != null) {
+                api.setCategoryId(updateApiDto.categoryId());
+            }
+
+            if (updateApiDto.name() != null) {
+                api.setName(updateApiDto.name());
+            }
+
+            if (updateApiDto.description() != null) {
+                api.setDescription(updateApiDto.description());
+            }
+
+            if (updateApiDto.price() != null) {
+                api.setPrice(updateApiDto.price());
+            }
+
+            if (updateApiDto.providerId() != null) {
+                api.setProviderId(updateApiDto.providerId());
+            }
+
+            apiRepository.save(api);
+        }
+    }
+
+    public void deleteById(String apiId) {
+        var id = UUID.fromString(apiId);
+
+        var apiExists = apiRepository.existsById(id);
+
+        if (apiExists) {
+            apiRepository.deleteById(id);
+        }
     }
 }
