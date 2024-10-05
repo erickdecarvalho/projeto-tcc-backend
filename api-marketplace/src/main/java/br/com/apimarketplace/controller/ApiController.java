2c830923cd325b4023fd533a78003d49ac467cbf
Luiz Calixto <calixtoluiz0@gmail.com>
2024-10-05 08:27:19 -0300

    feat: Swagger implementation and APIController documentation provider

--- api-marketplace/src/main/java/br/com/apimarketplace/controller/ApiController.java
+++ api-marketplace/src/main/java/br/com/apimarketplace/controller/ApiController.java
@@ -7,18 +7,27 @@ import br.com.apimarketplace.dto.UpdateApiDto;
 import br.com.apimarketplace.model.Api;
 import br.com.apimarketplace.service.ApiCategoryService;
 import br.com.apimarketplace.service.ApiService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import org.apache.coyote.BadRequestException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
-
+@Tag(name = "API Controller", description = "Operações relacionadas às APIs")
+@Validated
 @RestController
 @RequestMapping("/apis")
 public class ApiController {
 
-    private ApiService apiService;
-    private ApiCategoryService apiCategoryService;
+    private final ApiService apiService;
+    private final ApiCategoryService apiCategoryService;
 
     public ApiController(ApiService apiService, ApiCategoryService apiCategoryService) {
         this.apiService = apiService;
@@ -26,43 +35,77 @@ public class ApiController {
     }
 
     @PostMapping
-    public ResponseEntity<Api> createApi(@RequestBody CreateApiDto createApiDto) {
-        var apiId = apiService.createApi(createApiDto);
-        return ResponseEntity.created(URI.create("/apis/" + apiId.toString())).build();
+    @Operation(summary = "Find an API by Id", description = "A provider can find an API using the API id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    public ResponseEntity<Api> createApi(@RequestBody @Valid CreateApiDto createApiDto){
+            var apiId = apiService.createApi(createApiDto);
+            return ResponseEntity.created(URI.create("/apis/" + apiId.toString())).build();
     }
 
     @GetMapping("/{apiId}")
-    public ResponseEntity<ApiResponseDto> getApiById(@PathVariable("apiId") String apiId) {
+    @Operation(summary = "Find an API by Id", description = "A provider can find an API using the API id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "404", description = "API not found"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    public ResponseEntity<ApiResponseDto> getApiById(@PathVariable("apiId") @NotBlank String apiId) {
         var api = apiService.getApiById(apiId);
-
-        if (api.isPresent()) {
-            return ResponseEntity.ok(api.get());
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return api.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+
     @GetMapping
+    @Operation(summary = "Find an API by Id", description = "A provider can find an API using the API id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     public ResponseEntity<List<ApiResponseDto>> listApis() {
         var apis = apiService.listAllApis();
-
         return ResponseEntity.ok(apis);
     }
 
     @PutMapping("/{apiId}")
-    public ResponseEntity<Void> updateApi(@PathVariable("apiId") String id, @RequestBody UpdateApiDto updateApiDto) {
+    @Operation(summary = "Update an API by Id", description = "A provider can update a API in User control")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "success created"),
+            @ApiResponse(responseCode = "404", description = "API not found"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    public ResponseEntity<Void> updateApi(@PathVariable("apiId") String id, @RequestBody @Valid UpdateApiDto updateApiDto) {
         apiService.updateApiById(id, updateApiDto);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{apiId}")
-    public ResponseEntity<Void> deleteApi(@PathVariable("apiId") String id) {
+    @Operation(summary = "Delete an API by Id", description = "A provider can delete a API in User control")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "success created"),
+            @ApiResponse(responseCode = "404", description = "API not found"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    public ResponseEntity<Void> deleteApi(@PathVariable("apiId") @NotBlank String id) {
         apiService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 
     @PostMapping("/categorias")
-    public ResponseEntity<Api> createApiCategory(@RequestBody CreateApiCategory createApiCategory) {
+    @Operation(summary = "Update an API by Id", description = "A provider can update a API in User control")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "success created"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    public ResponseEntity<Api> createApiCategory(@RequestBody @Valid CreateApiCategory createApiCategory) {
         var apiCategoryId = apiCategoryService.createCategory(createApiCategory);
         return ResponseEntity.created(URI.create("/apis/categorias" + apiCategoryId.toString())).build();
     }
